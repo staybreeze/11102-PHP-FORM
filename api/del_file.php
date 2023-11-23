936c4c48d0356c41ee46acfab5b26fed35f12b94
staybreeze <wunshengliao@gmail.com>
2023-11-23 11:52:34 +0800

    refactor(delete): 改進刪除功能以處理既存檔案

--- api/del_file.php
+++ api/del_file.php
@@ -3,6 +3,13 @@ include_once "../db.php";
 
 $id=$_GET['id'];
 
+// 給予unlink刪除
+$id=$_GET['id'];
+$file=find('files',$id)['name'];
+
 del('files',$id);
+
+unlink('../imgs/'.$file);
+
 
 header("location:../manage.php");
