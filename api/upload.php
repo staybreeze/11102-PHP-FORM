b7a6538339d6126d9f8457b8d6d5a23203e9a2be
staybreeze <wunshengliao@gmail.com>
2023-11-23 10:18:26 +0800

    chore:加入BS5美化畫面

--- api/upload.php
+++ api/upload.php
@@ -1,20 +1,28 @@
 <?php
-
-echo $_POST['name'];
-echo "<br>";
+include_once "../db.php";
+/* echo $_POST['name'];
+echo "<br>"; */
 if(!empty($_FILES['img']['tmp_name'])){
-    echo $_FILES['img']['tmp_name'];
+/*     echo $_FILES['img']['tmp_name'];
     echo "<br>";
     echo $_FILES['img']['name'];
     echo "<br>";
     echo $_FILES['img']['type'];
     echo "<br>";
-    echo $_FILES['img']['size'];
+    echo $_FILES['img']['size']; */
     $tmp=explode(".",$_FILES['img']['name']);
     $subname=".".end($tmp);
     $filename=date("YmdHis").rand(10000,99999).$subname;
     move_uploaded_file($_FILES['img']['tmp_name'],"../imgs/".$filename);
-    header("location:../upload.php?img=".$filename);
+
+    $file=['name'=>$filename,
+            'type'=>$_FILES['img']['type'],
+            'size'=>$_FILES['img']['size'],
+            'desc'=>$_POST['desc']];
+
+    insert('files',$file);
+    //header("location:../upload.php?img=".$filename);
+    header("location:../manage.php");
 }else{
     header("location:../upload.php?err=上傳失敗");
 }
--- manage.php
+++ manage.php
@@ -5,7 +5,7 @@
  * 3.取得檔案資訊並寫入資料表
  * 4.製作檔案管理功能頁面
  */
-
+include_once "db.php";
 
 ?>
 <!DOCTYPE html>
@@ -16,19 +16,50 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案管理功能</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
 <h1 class="header">檔案管理練習</h1>
 <!----建立上傳檔案表單及相關的檔案資訊存入資料表機制----->
-
+<h3><a href="upload.php">上傳檔案</a></h3>
 
 
 
 
 <!----透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作----->
+<?php
+$files=all('files');
+
+?>
+<div class="col-8 mx-auto">
+    <table class='table table-success'>
+        <tr>
+            <td>id</td>
+            <td>檔名</td>
+            <td>類型</td>
+            <td>大小</td>
+            <td>描述</td>
+            <td>上傳時間</td>
+        </tr>
+    <?php
+    foreach($files as $file){
+    ?>
+        <tr>
+            <td><?=$file['id'];?></td>
+            <td><?=$file['name'];?></td>
+            <td><?=$file['type'];?></td>
+            <td><?=$file['size'];?></td>
+            <td><?=$file['desc'];?></td>
+            <td><?=$file['create_at'];?></td>
+        </tr>
+    <?php
+    }
+    ?>    
+    </table>
+</div>
 
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
